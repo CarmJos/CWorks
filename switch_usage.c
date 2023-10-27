a4466cffa1ec27e9a9f0429d1421780d94df0e6a
Carm <carm@carm.cc>
2023-10-27 10:34:49 +0800

    feat(code): Up-to-date

---
+++ switch_usage.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int score;
+    printf("Please input the score: \n");
+    scanf("%d", &score);
+
+    if (score < 0 || score > 100) {
+        printf("Not a illegal score.");
+        return 0;
+    }
+
+    int level = score / 10;
+    switch (level) {
+        case 9:
+            printf("A");
+            break;
+        case 8:
+            printf("B");
+            break;
+        case 7:
+            printf("C");
+            break;
+        case 6:
+            printf("D");
+            break;
+        default:
+            printf("E");
+            break;
+    }
+
+    return 0;
+}
