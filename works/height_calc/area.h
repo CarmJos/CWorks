65916f060bd839b5c91a549f862904efede0029d
Carm <carm@carm.cc>
2023-11-03 09:02:41 +0800

    feat(code): finished p109,12 with structure method

--- works/height_calc/area.h
+++ works/height_calc/area.h
@@ -10,12 +10,16 @@ typedef struct circle {
     double radius;
 } Circle;
 
+double square(double x) {
+    return x * x;
+}
+
 // calculate the squared distance of two points
-double distanceSquare(Point p1, Point p2) {
-    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+double distanceSquared(Point p1, Point p2) {
+    return square((p1.x - p2.x)) + square((p1.y - p2.y));
 }
 
 // predicate the point(x, y) whether inside the circle(circleX, circleY, circleRadius)
 int isInside(Point point, Circle circle) {
-    return distanceSquare(point, circle.center) < circle.radius * circle.radius;
+    return distanceSquared(point, circle.center) < square(circle.radius);
 }
