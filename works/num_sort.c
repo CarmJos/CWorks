aacc4183a92eea9f7f6a11b55fc5af8fb67bc692
Carm <carm@carm.cc>
2023-11-03 10:06:37 +0800

    feat(code): update sort method

--- works/num_sort.c
+++ works/num_sort.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 
+void swap(int *x, int *y) {
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+}
+
 int main() {
 
     int a, b, c, t;
     printf("Please input 3 numbers(split by space): \n");
     scanf("%d %d %d", &a, &b, &c);
 
-    if (b < a) {
-        t = a;
-        a = b;
-        b = t;
-    }
-
-    if (c < a) {
-        t = a;
-        a = c;
-        c = t;
-    }
-    if (c < b) {
-        t = b;
-        b = c;
-        c = t;
-    }
+    if (b < a) swap(&a, &b);
+    if (c < a) swap(&a, &c);
+    if (c < b) swap(&b, &c);
 
     printf("%d < %d < %d", a, b, c);
     return 0;
