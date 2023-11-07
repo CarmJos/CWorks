a9fb05dd8d7d3a402fb692f20574589bd1bf8ce7
Carm <carm@carm.cc>
2023-11-07 15:20:47 +0800

    feat(code): finished P138,10

---
+++ works/rebound_height.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    double height = 100;
+    double sum = 0;
+
+    for (int i = 0; i < 10; ++i) {
+        sum += height * (i > 0 ? 2 : 1);
+        height /= 2;
+        printf(
+                "#%d -> sum=%.2lf / rebound=%.2lf  \n",
+                i + 1, sum, height
+        );
+    }
+}
