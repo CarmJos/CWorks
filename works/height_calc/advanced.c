1b782ea070ca22a261e617de4c45e777e891374f
carm <carm@carm.cc>
2023-11-10 16:17:01 +0800

    docs(setup): Add settings documentation.

--- works/height_calc/advanced.c
+++ works/height_calc/advanced.c
@@ -6,21 +6,19 @@ int main() {
     printf("Please input the point (format: x,y ): \n");
     scanf("%f,%f", &x, &y);
 
-    Point point = {x, y};
-    Circle circle1 = {{2, 2}, 1};
-    Circle circle2 = {{2, -2}, 1};
-    Circle circle3 = {{-2, 2}, 1};
-    Circle circle4 = {{-2, -2}, 1};
+    const Point point = {x, y};
+    const Circle circle1 = {{2, 2}, 1};
+    const Circle circle2 = {{2, -2}, 1};
+    const Circle circle3 = {{-2, 2}, 1};
+    const Circle circle4 = {{-2, -2}, 1};
 
     if (isInside(point, circle1) || isInside(point, circle2)
         || isInside(point, circle3) || isInside(point, circle4)) {
         printf("Height = 10.0\n");
-    } else {
+    }
+    else {
         printf("Height = 0.0\n");
     }
 
     return 0;
 }
-
-
-
