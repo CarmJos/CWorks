d290e8ced278905a0dffbd91d6699afb69a5214f
carm <carm@carm.cc>
2023-12-07 23:19:16 +0800

    feat(calc): finished calculators

--- works/calculator/legendre.c
+++ works/calculator/legendre.c
@@ -4,7 +4,7 @@
 // n=0, 1
 // n=1, Pn(x)  =     x
 // n>=1 ( [(2n-1) * x] * Pn-1(x) - [(n-1) * Pn-2(x)] ) / n
-static double calc(const int n, const double x) {
+static double calc_legendre(const int n, const double x) {
     if (n == 0) return 1;
     if (n == 1) return x;
     return ((2.0 * n - 1.0) * x * calc(n - 1, x) - (n - 1.0) * calc(n - 2, x)) / n;
@@ -17,6 +17,6 @@ int main() {
     printf("Please input the (n,x): \n");
     scanf("%d,%lf", &n, &x);
 
-    printf("Result -> %lf", calc(n, x));
+    printf("Result -> %lf", calc_legendre(n, x));
     return 0;
 }
