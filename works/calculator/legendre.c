413598f06039e502ce9f4b14148dd365360e8f54
carm <carm@carm.cc>
2023-12-07 23:18:54 +0800

    feat(calc): finished calculators

--- works/calculator/legendre.c
+++ works/calculator/legendre.c
@@ -3,8 +3,8 @@
 // calculate legendre's n, x value
 // n=0, 1
 // n=1, Pn(x)  =     x
-// n>=1 ( [(2n-1) * x] - Pn-1(x) - [(n-1) * Pn-2(x)] ) / n
-static double calc(long long int n, double x) {
+// n>=1 ( [(2n-1) * x] * Pn-1(x) - [(n-1) * Pn-2(x)] ) / n
+static double calc(const int n, const double x) {
     if (n == 0) return 1;
     if (n == 1) return x;
     return ((2.0 * n - 1.0) * x * calc(n - 1, x) - (n - 1.0) * calc(n - 2, x)) / n;
