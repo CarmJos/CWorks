3ef4928469b37d405762ab86e9cbb923549e438e
carm <carm@carm.cc>
2023-12-19 17:30:01 +0800

    chore(calc): update calculators

--- works/calculator/change_calc.c
+++ works/calculator/change_calc.c
@@ -9,28 +9,33 @@ static const int DENOMINATIONS[] = {
     500, 200, 100, // 5元, 2元, 1元
     50, 20, 10, 5, 2, 1 // 5角, 2角, 1角, 5分, 2分, 1分
 };
+static const int DENOMINATIONS_SIZE = sizeof(DENOMINATIONS) / sizeof(int);
 
 void change(const double price, const double paid) {
-    double change = paid - price;
+    double change = paid - price; // 计算须找零金额
     printf("Change: %.2lf\n", change);
 
     if (change <= 0) {
+        // 付款不足或无需找零
         printf("No any change.\n");
         return;
     }
 
-    int remain = (int)(change * 100);
-    int amount[12] = {0};
+    int remain = (int)(change * 100); // 以分为单位的找零金额
+    int amount[DENOMINATIONS_SIZE] = {0}; // 每个面额的张数
 
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < DENOMINATIONS_SIZE; i++) {
+        // 从大面额开始找零，循环每一个面额
         while (remain >= DENOMINATIONS[i]) {
-            remain -= DENOMINATIONS[i];
-            amount[i]++;
+            // 当前面额的金额仍然可以找零
+            remain -= DENOMINATIONS[i]; // 扣除当前面额的金额
+            amount[i]++; // 此面额的对应张数加一
         }
     }
 
-    for (int i = 0; i < 12; i++) {
-        if (amount[i] <= 0)continue;
+    for (int i = 0; i < DENOMINATIONS_SIZE; i++) {
+        if (amount[i] <= 0) continue; // 无需输出的面额直接跳过
+        // 输出当前面额的金额(单位转换为元)和对应张数
         printf("# CNY %.2f × %d\n", DENOMINATIONS[i] / 100.0, amount[i]);
     }
 }
