aa939422532d75527e8d721c208caa6b04b87012
carm <carm@carm.cc>
2023-12-19 15:56:39 +0800

    feat(calc): finished calculators

--- works/calculator/score_calc.c
+++ works/calculator/score_calc.c
@@ -6,15 +6,18 @@ static bool closer(const double avg, const double a, const double b) {
     return fabs(avg - a) < fabs(avg - b);
 }
 
-static bool farther(const double avg, const double a, const double b) {
-    return fabs(avg - a) > fabs(avg - b);
-}
-
 int main() {
     double scores[10];
-    printf("Please input the scores: \n");
+    printf("Please input the scores (1-100): \n");
     for (int i = 0; i < 10; i++) {
         scanf("%lf", &scores[i]);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (scores[i] < 0 || scores[i] > 100) {
+            printf("Invalid score: %.2lf\n", scores[i]);
+            return 1;
+        }
     }
 
     double min = -1, max = -1, total = 0;
@@ -28,18 +31,18 @@ int main() {
     printf("Min score is %.2lf, skipped.\n", min);
     printf("Average score is %.2lf\n", avg);
 
-    double closest = -1, farthests = -1;
+    double closest = -1, farthest = -1;
     for (int i = 0; i < 10; i++) {
         if (scores[i] == min || scores[i] == max) continue;
         if (closest == -1 || closer(avg, scores[i], closest)) {
             closest = scores[i];
         }
-        if (farthests == -1 || farther(avg, scores[i], farthests)) {
-            farthests = scores[i];
+        if (farthest == -1 || !closer(avg, scores[i], farthest)) {
+            farthest = scores[i];
         }
     }
-
     printf("Closest score is %.2lf\n", closest);
-    printf("Farthest score is %.2lf\n", farthests);
+    printf("Farthest score is %.2lf\n", farthest);
+
     return 0;
 }
