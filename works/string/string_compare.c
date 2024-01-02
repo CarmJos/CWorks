724727d2f24bd5f0aa1224e7ca4442e987c1579c
Carm <carm@carm.cc>
2024-01-02 15:06:14 +0800

    feat(compare): Finished string comparator

---
+++ works/string/string_compare.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int strcmp(char *str1, char *str2) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) return str1[i] - str2[i];
+        i++;
+    }
+    return str1[i] - str2[i];
+}
+
+int main() {
+    char str1[50] = {0};
+    char str2[50] = {0};
+
+    printf("Input 2 parts of char array: \n");
+    fgets(str1, 50, stdin);
+    fgets(str2, 50, stdin);
+
+    printf("Result ->  %d", strcmp(str1, str2));
+
+    return 0;
+}
