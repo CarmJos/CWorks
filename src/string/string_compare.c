21c0fef461182dbcb09544776b6e5e7701da7372
Carm <carm@carm.cc>
2024-01-02 15:15:13 +0800

    feat(compare): Finished string comparator

--- src/string/string_compare.c
+++ src/string/string_compare.c
@@ -2,7 +2,7 @@
 
 int strcmp(char *str1, char *str2) {
     int i = 0;
-    while (str1[i] != '\0' && str2[i] != '\0') {
+    while (str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i]) {
         i++;
     }
     return str1[i] - str2[i];
