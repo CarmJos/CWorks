f69513fb1ffc6940707f7a424c22307ec0ecc975
Carm <carm@carm.cc>
2024-03-14 11:40:57 +0800

    chore(licence): Add licence and copyright comments

--- src/string/string_count.c
+++ src/string/string_count.c
@@ -1,3 +1,19 @@
+// This project is a part of CWorks (C language practice project).
+//     Copyright (C) 2024. CarmJos
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // 有一篇文章，三行文字，每行80个字符，统计其中的英文字符大写、英文字符小写、空格、数字和其他字符的个数。
 
 #include <stdio.h>
