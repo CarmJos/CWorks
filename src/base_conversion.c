85437e8146665118fe7ff59f28eafc817cd2c0f0
Carm <carm@carm.cc>
2024-04-11 11:06:34 +0800

    feat(math): Finished homework

---
+++ src/base_conversion.c
@@ -0,0 +1,57 @@
+// This project is a part of CWorks (C language practice project).
+//     Copyright (C) 2024. CarmJos
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//
+// Created by Karmu on 2024/4/11.
+//
+
+#include "list/linked_stack.h"
+
+// 使用栈实现进制转换
+
+char *convert(int num, int base) {
+
+    Stack *stack = s_create(100);
+    while (num != 0) {
+        s_push(stack, num % base);
+        num /= base;
+    }
+
+    char *result = (char *) malloc(100 * sizeof(char));
+    int index = 0;
+    while (!s_empty(stack)) {
+        result[index++] = s_pop(stack) + '0';
+    }
+    result[index] = '\0';
+    free(stack);
+    return result;
+}
+
+int main() {
+
+    int num, base;
+    printf("Please input a number: ");
+    scanf("%d", &num);
+
+    printf("\n");
+    printf("Please input the base: ");
+    scanf("%d", &base);
+
+    char *result = convert(num, base);
+    printf("%s\n", result);
+    free(result);
+    return 0;
+}
---
+++ src/bracket_matching.c
@@ -0,0 +1,54 @@
+// This project is a part of CWorks (C language practice project).
+//     Copyright (C) 2024. CarmJos
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//
+// Created by Karmu on 2024/4/11.
+//
+#include <stdbool.h>
+#include "list/linked_stack.h"
+
+// 使用栈实现前后括号匹配（支持括号、中括号、大括号）
+static bool matching(const char *input) {
+    Stack *stack = s_create(100);
+    for (int i = 0; input[i] != '\0'; i++) {
+        char c = input[i];
+        if (c == '(' || c == '[' || c == '{') {
+            s_push(stack, c);
+        } else if (c == ')' || c == ']' || c == '}') {
+            if (s_empty(stack)) return false;
+            char top = s_pop(stack);
+            if (c == ')' && top != '(') return false;
+            if (c == ']' && top != '[') return false;
+            if (c == '}' && top != '{') return false;
+        }
+    }
+    return s_empty(stack);
+}
+
+int main() {
+
+    char input[30];
+    printf("Please input the values: ");
+    gets(input);
+
+    if (matching(input)) {
+        printf("Matching!\n");
+    } else {
+        printf("Not matching!\n");
+    }
+
+    return 0;
+}
